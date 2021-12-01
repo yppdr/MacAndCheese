2e77f03ff4932d0eec96635dc5a809c89e0d9cab
Yannis Piot Pilot <yannis@piotpilot.eu>
2021-12-01 18:19:09 +0100

    WIP

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installation de Homebrew"
+
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh
+
+echo "Installation de Firefox Dev, Alfred 3, SublimeText, IntelliJ IDEA, Docker, GitKraken, iTerm2, AppCleaner, Spotify, VLC, Telegram et Discord"
+
+curl -s 'https://api.macapps.link/fr/firefoxdev-alfred-sublime-intellij-docker-gitkraken-iterm-appcleaner-spotify-vlc-telegram-discord' | sh
+
+echo "Installation done! SHEEEEEEEEEEESH"
